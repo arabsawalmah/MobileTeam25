dc0314c88223eab9f159039af3a93e4498b779b2
Arab Sawalmah <s12326636@stu.najah.edu>
2026-04-16 00:38:54 +0300

    first arab commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import HomeType from "./HomeType";
+
+export default function HomeScreen() {
+
+  return (
+    <SafeAreaView>
+      <HomeType/>
+    </SafeAreaView>
+  );
+}
+
+
